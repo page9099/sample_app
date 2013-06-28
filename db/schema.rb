bb8e0fd13ad2c1ff474be83d3984db2bd1528cfe
page9099 <page9099@gmail.com>
2013-06-28 21:25:15 +0900

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626134909) do
+ActiveRecord::Schema.define(:version => 20130627141446) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
